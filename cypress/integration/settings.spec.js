470856a0a06ecb0bba6608974b8ceb332cd75bbd
David.Vader <david.vader@target.com>
2019-11-22 13:08:59 -0600

    fixing test by forcing click on covered checkboxes

--- cypress/integration/settings.spec.js
+++ cypress/integration/settings.spec.js
@@ -59,13 +59,13 @@ context("Repo Settings", () => {
       it("clicking allow_push checkbox should toggle the value", () => {
         cy.get("[data-test=repo-checkbox-allow_push] input").as("allowPushCheckbox");
         cy.get("@allowPushCheckbox").should("have.checked");
-        cy.get("@allowPushCheckbox").click();
+        cy.get("@allowPushCheckbox").click({ force: true });
         cy.get("@allowPushCheckbox").should("not.have.checked");
       });
       it("clicking access radio should toggle both values", () => {
         cy.get("[data-test=repo-radio-private] input").as("accessRadio");
         cy.get("@accessRadio").should("not.have.checked");
-        cy.get("@accessRadio").click();
+        cy.get("@accessRadio").click({ force: true });
         cy.get("@accessRadio").should("have.checked");
       });
       it("build timeout input should allow number input", () => {
@@ -95,7 +95,7 @@ context("Repo Settings", () => {
         cy.get("@repoTimeoutInput").type("80");
         cy.get("[data-test=repo-timeout] button")
           .should("be.visible")
-          .click();
+          .click({ force: true });
         cy.get("[data-test=repo-timeout] button").should("be.disabled");
       });
       it("clicking Refresh Settings button should clear input", () => {
@@ -105,7 +105,7 @@ context("Repo Settings", () => {
           .type("123");
         cy.get("[data-test=refresh-repo-settings]")
           .should("be.visible")
-          .click();
+          .click({ force: true });
         cy.get("@repoTimeoutInput").should("have.value", "60");
       });
     });
