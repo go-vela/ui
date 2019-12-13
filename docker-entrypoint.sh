0f517f27c0d6ce01fcfbf6738219fc65a50492e2
David May <1301201+wass3r@users.noreply.github.com>
2019-12-13 19:45:28 +0000

    feat: add security headers and login proxy (#42)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,4 +7,8 @@ do
     envsubst '${VELA_API},${VELA_SOURCE_URL},${VELA_SOURCE_CLIENT},${VELA_FEEDBACK_URL},{$VELA_DOCS_URL}' < "$f" > "$f".tmp && mv "$f".tmp "$f"
 done
 
+NGINX_CONF=/etc/nginx/conf.d/default.conf
+# shellcheck disable=SC2016
+envsubst '${VELA_API}' < "$NGINX_CONF" > "$NGINX_CONF".tmp && mv "$NGINX_CONF".tmp "$NGINX_CONF"
+
 exec "$@"
