39a234848ac262bd80ba4492a8cf44d71b511f32
David.Vader <david.vader@target.com>
2020-02-06 16:05:19 -0600

    redesigned using details/summary

--- src/elm/Help.elm
+++ src/elm/Help.elm
@@ -6,73 +6,124 @@ Use of this source code is governed by the LICENSE file in this repository.
 
 module Help exposing (view)
 
+import Dict exposing (Dict)
 import FeatherIcons
-import Html exposing (Html, button, code, div, input, li, text)
+import Html exposing (Html, button, code, details, div, input, li, summary, text)
 import Html.Attributes exposing (attribute, class, id, type_, value)
 import Html.Events exposing (onClick)
 import Pages exposing (Page(..))
 import Util
+import Vela exposing (BuildNumber, Org, Repo)
+
+
+type alias Command =
+    { name : String
+    , content : String
+    }
+
+
+type alias Commands =
+    List Command
 
 
 view : Page -> Bool -> msg -> Html msg
 view page show toggle =
-    li [ class "cli-help" ]
-        [ button
-            [ Util.testAttribute "cli-help"
-            , onClick toggle
+    li [ class "contextual-help" ]
+        [ details [ class "details", class "-no-pad", attribute "role" "button", Util.open show, onClick toggle ]
+            [ summary [ class "summary", class "-no-pad" ]
+                [ FeatherIcons.terminal |> FeatherIcons.withSize 18 |> FeatherIcons.toHtml []
+                ]
+            , cliHelp show <| pageToHelp page
+            ]
+        ]
+
+
+
+-- details [ class "details", class "-marker-right", class "-no-pad", class "identity-name", attribute "role" "navigation" ]
+--     [ summary [ class "summary" ]
+--         [ text session.username
+--         , FeatherIcons.chevronDown |> FeatherIcons.withSize 20 |> FeatherIcons.withClass "details-icon-expand" |> FeatherIcons.toHtml []
+--         ]
+--     , ul [ class "identity-menu", attribute "aria-hidden" "true", attribute "role" "menu" ]
+--         [ li [ class "identity-menu-item" ]
+--             [ a [ Routes.href Routes.Logout, Util.testAttribute "logout-link", attribute "role" "menuitem" ] [ text "Logout" ] ]
+--         ]
+--     ]
+-- button
+--     [ Util.testAttribute "contextual-help"
+--     , onClick toggle
+--     , attribute "aria-label" "view cli command for this page"
+--     , class "button"
+--     , class "-icon"
+--     , class "-white"
+--     , class "contextual-help-button"
+--     ]
+--     [ FeatherIcons.terminal |> FeatherIcons.withSize 18 |> FeatherIcons.toHtml [] ]
+-- , cliHelp show <| pageToHelp page
+
+
+cliHelp : Bool -> Commands -> Html msg
+cliHelp show commands =
+    div [ class "contextual-help-tooltip" ] <|
+        [ div [ class "-arrow" ] []
+        , div [ class "-header" ] [ code [] [ text "View this page using the CLI" ] ]
+        ]
+            ++ List.map toHelp commands
+
+
+toHelp : Command -> Html msg
+toHelp command =
+    div []
+        [ text command.name
+        , code [ class "-command" ] [ text command.content ]
+        , copyButton
+            [ Util.testAttribute "contextual-help"
             , attribute "aria-label" "view cli command for this page"
             , class "button"
             , class "-icon"
             , class "-white"
             ]
-            [ FeatherIcons.terminal |> FeatherIcons.withSize 18 |> FeatherIcons.toHtml [] ]
-        , cliHelp show <| pageToHelp page
+            command.content
         ]
 
 
-pageToHelp : Page -> String
-pageToHelp page =
-    case page of
-        Pages.RepositoryBuilds org repo _ _ ->
-            "vela get builds --org " ++ org ++ " --repo " ++ repo
-
-        _ ->
-            "unknown page"
-
-
-cliHelp : Bool -> String -> Html msg
-cliHelp show command =
-    if show then
-        div [ class "-tooltip" ]
-            [ div [ class "-arrow" ] []
-            , div [ class "-header" ] [ code [] [ text "View this page using the CLI" ] ]
-            , div []
-                [ code [ class "-command" ] [ text command ]
-                , copyButton
-                    [ Util.testAttribute "cli-help"
-                    , attribute "aria-label" "view cli command for this page"
-                    , class "button"
-                    , class "-icon"
-                    , class "-white"
-                    ]
-                    (FeatherIcons.copy
-                        |> FeatherIcons.withSize 18
-                        |> FeatherIcons.toHtml []
-                    )
-                    command
-                ]
-            ]
-
-    else
-        text ""
-
-
-copyButton : List (Html.Attribute msg) -> Html msg -> String -> Html msg
-copyButton attributes markdown copyText =
+copyButton : List (Html.Attribute msg) -> String -> Html msg
+copyButton attributes copyText =
     button
         (attributes
             ++ [ class "copy-button"
                , attribute "data-clipboard-text" copyText
                ]
         )
-        [ markdown ]
+        [ FeatherIcons.copy
+            |> FeatherIcons.withSize 18
+            |> FeatherIcons.toHtml []
+        ]
+
+
+builds : Org -> Repo -> Command
+builds org repo =
+    Command "list builds" <| "vela get builds --org " ++ org ++ " --repo " ++ repo
+
+
+build : Org -> Repo -> BuildNumber -> Command
+build org repo buildNumber =
+    Command "get build" <| "vela get build --org " ++ org ++ " --repo " ++ repo ++ " --build " ++ buildNumber
+
+
+unknown : Command
+unknown =
+    Command "" "unknown page"
+
+
+pageToHelp : Page -> Commands
+pageToHelp page =
+    case page of
+        Pages.RepositoryBuilds org repo _ _ ->
+            [ builds org repo ]
+
+        Pages.Build org repo buildNumber _ ->
+            [ build org repo buildNumber ]
+
+        _ ->
+            [ unknown ]
