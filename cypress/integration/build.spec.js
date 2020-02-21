46a4d5ecea5599e885ddfefeb2ef8e4054401515
David May <david.may@target.com>
2020-02-21 00:39:57 -0600

    chore: add licenses to test files

--- cypress/integration/build.spec.js
+++ cypress/integration/build.spec.js
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+ * Use of this source code is governed by the LICENSE file in this repository.
+ */
+
 context('Build', () => {
   context('logged in and server returning build error', () => {
     beforeEach(() => {
