38f033cad8feab2a3710704f4efa4449b28c4726
davidvader <davelvader@gmail.com>
2020-03-03 09:23:53 -0600

    add secret and update secret inputs

--- src/elm/Pages/RepoSecrets.elm
+++ src/elm/Pages/RepoSecrets.elm
@@ -6,16 +6,13 @@ Use of this source code is governed by the LICENSE file in this repository.
 
 module Pages.RepoSecrets exposing (Msgs, view)
 
+import FeatherIcons
 import Html
     exposing
         ( Html
-        , br
+        , code
         , details
         , div
-        , em
-        , h2
-        , p
-        , section
         , span
         , summary
         , text
@@ -46,20 +43,26 @@ type alias Msgs msg =
 -}
 view : WebData Secrets -> Html msg
 view secrets =
-    let
-        secrets_ =
-            case secrets of
-                Success s ->
-                    if List.length s > 0 then
-                        viewSecrets s
+    case secrets of
+        Success s ->
+            if List.length s > 0 then
+                div [] [ addSecret, viewSecrets s ]
 
-                    else
-                        div [] [ text "no secrets found for this repository" ]
+            else
+                div [] [ text "no secrets found for this repository" ]
 
-                _ ->
-                    div [] [ largeLoader ]
-    in
-    secrets_
+        _ ->
+            div [] [ largeLoader ]
+
+
+addSecret : Html msg
+addSecret =
+    div [ class "add-secret" ]
+        [ div [ class "label" ] [ text "Add Secret" ]
+        , div [] [ Html.input [ class "secret-name", Html.Attributes.placeholder "Secret Name" ] [] ]
+        , div [] [ Html.textarea [ class "secret-value", Html.Attributes.placeholder "Secret Value" ] [] ]
+        , div [] [ Html.button [ class "button", class "-outline" ] [ text "Add" ] ]
+        ]
 
 
 viewSecrets : Secrets -> Html msg
@@ -79,9 +82,10 @@ secretsTable secrets =
 headers : Html msg
 headers =
     div [ class "headers" ]
-        [ div [ class "header" ] [ text "name" ]
-        , div [ class "header" ] [ text "type" ]
+        [ div [ class "first-cell", class "table-label" ] [ text "Secrets" ]
+        , div [ class "header" ] [ text "name" ]
         , div [ class "header" ] [ text "events" ]
+        , div [ class "header" ] [ text "images" ]
         ]
 
 
@@ -97,7 +101,7 @@ rows secrets =
 row : Secret -> Html msg
 row secret =
     details [ class "details", class "-no-pad", Util.testAttribute "secret" ]
-        [ summary [ class "summary", class "-no-pad" ]
+        [ summary [ class "summary" ]
             [ preview secret ]
         , info secret
         ]
@@ -108,10 +112,22 @@ row secret =
 preview : Secret -> Html msg
 preview secret =
     div [ class "row", class "preview" ]
-        [ cell secret.name <| class "host"
-        , cell secret.name <| class "event"
-        , cell secret.name <| class "branch"
+        [ firstCell
+        , cell secret.name <| class "host"
+        , arrayCell secret.events "no events"
+        , arrayCell secret.images "no images"
         ]
+
+
+arrayCell : List String -> String -> Html msg
+arrayCell images default =
+    div [ class "cell" ] <|
+        List.intersperse (text ",") <|
+            if List.length images > 0 then
+                List.map (\image -> code [ class "text", class "-m-l" ] [ text image ]) images
+
+            else
+                [ code [ class "text" ] [ text default ] ]
 
 
 {-| cell : takes text and maybe attributes and renders cell data for hooks table row
@@ -122,11 +138,27 @@ cell txt cls =
         [ span [] [ text txt ] ]
 
 
+{-| firstCell : renders the expansion chevron icon
+-}
+firstCell : Html msg
+firstCell =
+    div [ class "first-cell" ]
+        [ FeatherIcons.chevronDown |> FeatherIcons.withSize 20 |> FeatherIcons.withClass "details-icon-expand" |> FeatherIcons.toHtml []
+        ]
+
+
 {-| info : renders the table row details when clicking/expanding a row
 -}
 info : Secret -> Html msg
 info secret =
-    div [ class "loading" ] [ Util.smallLoaderWithText "loading secret..." ]
+    div [ class "update-secret" ]
+        [ div []
+            [ text "Update Secret "
+            , Html.code [ class "text" ] [ text secret.name ]
+            ]
+        , Html.textarea [ Html.Attributes.placeholder "Secret Value" ] []
+        , div [] [ Html.button [ class "button", class "-outline" ] [ text "Update" ] ]
+        ]
 
 
 viewSecret : Secret -> Html msg
