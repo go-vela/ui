47cf2540a6ec8e177fca4bafffa542885521a747
davidvader <davelvader@gmail.com>
2020-04-10 15:16:50 -0500

    in progress refactor

---
+++ src/elm/Pages/Secrets/UpdateSecret.elm
@@ -0,0 +1,14 @@
+{--
+Copyright (c) 2020 Target Brands, Inc. All rights reserved.
+Use of this source code is governed by the LICENSE file in this repository.
+--}
+
+
+module Pages.Secrets.UpdateSecret exposing (view)
+
+import Html exposing (Html, div)
+
+
+view : Html msg
+view =
+    div [] []
