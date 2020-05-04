66dda9417cc7529a6ca881285babf31df81e32d0
David Vader <48764154+davidvader@users.noreply.github.com>
2020-05-04 14:34:08 -0500

    fix(secrets): pull should be pull_request (#153)
    * pull should be pull_request
    
    * deploy should be deployment

--- src/elm/Pages/Secrets/Form.elm
+++ src/elm/Pages/Secrets/Form.elm
@@ -167,15 +167,15 @@ viewEventsSelect secretUpdate =
               <|
                 OnChangeEvent "push"
             , checkbox "Pull Request"
-                "pull"
-                (eventEnabled "pull" secretUpdate.events)
+                "pull_request"
+                (eventEnabled "pull_request" secretUpdate.events)
               <|
-                OnChangeEvent "pull"
+                OnChangeEvent "pull_request"
             , checkbox "Deploy"
-                "deploy"
-                (eventEnabled "deploy" secretUpdate.events)
+                "deployment"
+                (eventEnabled "deployment" secretUpdate.events)
               <|
-                OnChangeEvent "deploy"
+                OnChangeEvent "deployment"
             , checkbox "Tag"
                 "tag"
                 (eventEnabled "tag" secretUpdate.events)
