adcaada5b77a909b04bcbdd58d8c8ad941fa8719
davidvader <davelvader@gmail.com>
2020-06-26 16:36:53 -0500

    lint

--- cypress/integration/logs.spec.js
+++ cypress/integration/logs.spec.js
@@ -3,26 +3,26 @@
  * Use of this source code is governed by the LICENSE file in this repository.
  */
 context('visit Build with log line with fragment', () => {
-    beforeEach(() => {
-        cy.server();
-        cy.stubBuild();
-        cy.stubStepsWithANSILogs();
-        cy.login('/someorg/somerepo/1');
-        cy.get('[data-test=steps]').as('steps');
-        cy.get('[data-test=step]').as('step');
-        cy.get('[data-test=step-header]').as('stepHeaders');
-        cy.get('@stepHeaders').click({ force: true, multiple: true });
-        cy.get('[data-test=logs-1]').as('logs');
-        cy.get('@stepHeaders').click({ force: true, multiple: true });
-        cy.visit('/someorg/somerepo/1#step:2:2');
-        cy.reload();
+  beforeEach(() => {
+    cy.server();
+    cy.stubBuild();
+    cy.stubStepsWithANSILogs();
+    cy.login('/someorg/somerepo/1');
+    cy.get('[data-test=steps]').as('steps');
+    cy.get('[data-test=step]').as('step');
+    cy.get('[data-test=step-header]').as('stepHeaders');
+    cy.get('@stepHeaders').click({ force: true, multiple: true });
+    cy.get('[data-test=logs-1]').as('logs');
+    cy.get('@stepHeaders').click({ force: true, multiple: true });
+    cy.visit('/someorg/somerepo/1#step:2:2');
+    cy.reload();
+  });
+  it('line should be highlighted', () => {
+    cy.wait('@getLogs-2');
+    cy.get('[data-test=logs-2]').within(() => {
+      cy.get('[data-test=log-line-2]').as('line2:2');
+      cy.get('[data-test=log-line-num-2]').as('lineNumber2:2');
     });
-    it('line should be highlighted', () => {
-        cy.wait('@getLogs-2');
-        cy.get('[data-test=logs-2]').within(() => {
-            cy.get('[data-test=log-line-2]').as('line2:2');
-            cy.get('[data-test=log-line-num-2]').as('lineNumber2:2');
-        });
-        cy.get('@line2:2').should('have.class', '-focus');
-    });
+    cy.get('@line2:2').should('have.class', '-focus');
+  });
 });
