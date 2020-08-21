0a4ad0746df5b45cf2ab1a04bd15f02415a78a17
davidvader <davelvader@gmail.com>
2020-08-21 17:50:25 -0500

    lint

--- cypress/integration/logs.spec.js
+++ cypress/integration/logs.spec.js
@@ -15,13 +15,13 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
     cy.reload();
     cy.wait('@getLogs-2');
   });
-  
+
   it('line should not contain ansi characters', () => {
     cy.get('[data-test=log-line-2-30]').within(() => {
       cy.get('[class=ansi-red-fg]').should('not.exist');
     });
   });
-  
+
   it('line should contain ansi color css', () => {
     cy.get('[data-test=log-line-2-31]').within(() => {
       cy.get('[class=ansi-green-fg]').should('exist');
@@ -31,7 +31,7 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
       cy.get('[class=ansi-bright-black-fg]').should('exist');
     });
   });
-  
+
   it('ansi fg classes should change css color', () => {
     cy.get('[data-test=log-line-2-31]').within(() => {
       cy.get('[class=ansi-green-fg]')
@@ -44,7 +44,7 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
         .should('eq', 'rgb(235, 102, 117)');
     });
   });
-  
+
   it('line should respect ansi font style', () => {
     cy.get('[data-test=log-line-2-46]').within(() => {
       cy.get('.ansi-bold').should('exist');
@@ -52,10 +52,12 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
   });
 
   it('build should have collapse/expand actions', () => {
-    cy.get('[data-test=log-actions]').should('exist').within(() => {
-      cy.get('[data-test=collapse-all]').should('exist');
-      cy.get('[data-test=expand-all]').should('exist');
-    });
+    cy.get('[data-test=log-actions]')
+      .should('exist')
+      .within(() => {
+        cy.get('[data-test=collapse-all]').should('exist');
+        cy.get('[data-test=expand-all]').should('exist');
+      });
   });
 
   it('click collapse all should collapse all steps', () => {
@@ -72,22 +74,42 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
     cy.get('[data-test=collapse-all]').click({ force: true });
 
     // verify logs are hidden
-    cy.get('[data-test=step-header-1]').parent().should('not.have.attr', 'open');
-    cy.get('[data-test=step-header-2]').parent().should('not.have.attr', 'open');
-    cy.get('[data-test=step-header-3]').parent().should('not.have.attr', 'open');
-    cy.get('[data-test=step-header-4]').parent().should('not.have.attr', 'open');
-    cy.get('[data-test=step-header-5]').parent().should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-1]')
+      .parent()
+      .should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-2]')
+      .parent()
+      .should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-3]')
+      .parent()
+      .should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-4]')
+      .parent()
+      .should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-5]')
+      .parent()
+      .should('not.have.attr', 'open');
   });
   it('click expand all should expand all steps', () => {
     // close opened step
     cy.get('[data-test=step-header-2]').click({ force: true });
 
     // verify the steps are closed
-    cy.get('[data-test=step-header-1]').parent().should('not.have.attr', 'open');
-    cy.get('[data-test=step-header-2]').parent().should('not.have.attr', 'open');
-    cy.get('[data-test=step-header-3]').parent().should('not.have.attr', 'open');
-    cy.get('[data-test=step-header-4]').parent().should('not.have.attr', 'open');
-    cy.get('[data-test=step-header-5]').parent().should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-1]')
+      .parent()
+      .should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-2]')
+      .parent()
+      .should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-3]')
+      .parent()
+      .should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-4]')
+      .parent()
+      .should('not.have.attr', 'open');
+    cy.get('[data-test=step-header-5]')
+      .parent()
+      .should('not.have.attr', 'open');
 
     // collapse all
     cy.get('[data-test=expand-all]').click({ force: true });
@@ -123,42 +145,42 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
         cy.get('[data-test=download-logs-2]').should('not.exist');
       });
     });
-    
+
     it('should have trackers', () => {
       cy.get('[data-test=logs-2]').within(() => {
         cy.get('[data-test=bottom-log-tracker-2]').should('exist');
         cy.get('[data-test=top-log-tracker-2]').should('exist');
       });
     });
-    
+
     it('bottom tracker should not have focus', () => {
-      cy.focused().should('not.have.attr', 'data-test', 'bottom-log-tracker-2')
+      cy.focused().should('not.have.attr', 'data-test', 'bottom-log-tracker-2');
     });
 
     it('click jump to bottom should focus bottom tracker', () => {
       cy.get('[data-test=jump-to-bottom-2]').click({ force: true });
-      cy.focused().should('have.attr', 'data-test', 'bottom-log-tracker-2')
+      cy.focused().should('have.attr', 'data-test', 'bottom-log-tracker-2');
     });
 
     it('top tracker should not have focus', () => {
-      cy.focused().should('not.have.attr', 'data-test', 'top-log-tracker-2')
+      cy.focused().should('not.have.attr', 'data-test', 'top-log-tracker-2');
     });
 
     it('click jump to top should focus top tracker', () => {
       cy.get('[data-test=jump-to-top-2]').click({ force: true });
-      cy.focused().should('have.attr', 'data-test', 'top-log-tracker-2')
+      cy.focused().should('have.attr', 'data-test', 'top-log-tracker-2');
     });
 
     it('click follow logs should focus follow new logs', () => {
       // verify no prior focus
-      cy.focused().should('not.have.attr', 'data-test', 'bottom-log-tracker-2')
+      cy.focused().should('not.have.attr', 'data-test', 'bottom-log-tracker-2');
 
       // follow logs
       cy.get('[data-test=follow-logs-2]').first().click({ force: true });
 
       // wait for refresh and check for bottom focus
       cy.wait('@getLogs-2');
-      cy.focused().should('have.attr', 'data-test', 'bottom-log-tracker-2')
+      cy.focused().should('have.attr', 'data-test', 'bottom-log-tracker-2');
     });
   });
 
@@ -174,7 +196,7 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
         cy.get('[data-test=follow-logs-5]').should('exist');
       });
     });
-    
+
     it('bottom log actions should not exist', () => {
       cy.get('[data-test=bottom-log-actions-5]').should('not.exist');
     });
@@ -184,7 +206,7 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
     beforeEach(() => {
       cy.get('[data-test=step-header-1]').click({ force: true });
     });
-    
+
     it('top log actions should only contain follow', () => {
       cy.get('[data-test=top-log-actions-1]').within(() => {
         cy.get('[data-test=jump-to-bottom-1]').should('not.exist');
@@ -192,7 +214,7 @@ context('visit Build with ansi encoded logs using url line fragment', () => {
         cy.get('[data-test=follow-logs-1]').should('exist');
       });
     });
-    
+
     it('bottom log actions should not exist', () => {
       cy.get('[data-test=bottom-log-actions-1]').should('not.exist');
     });
