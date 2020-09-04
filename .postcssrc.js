db22dec9bebd9b06f7e8eb08dca85c1ee4b6cfca
davidvader <davelvader@gmail.com>
2020-09-04 10:54:48 -0500

    adding important to account for comment scrubbing

--- .postcssrc.js
+++ .postcssrc.js
@@ -15,7 +15,7 @@ const production = {
   plugins: [
     purgecss({
       content: ['./src/**/*.elm', './src/static/index.js'],
-      whitelist: ['html', 'body', 'svg'],
+      whitelist: ['html', 'body', 'svg', 'ansi-*'],
     }),
     autoprefixer,
   ],
