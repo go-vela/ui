cd468cdb38c2a43802d1a76e9d01c69898bef936
David May <david.may@target.com>
2021-02-05 10:50:35 -0600

    fix(proxy): switch to caddy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -eu
 
-for f in /usr/share/nginx/html/*.js
+for f in /srv/*.js
 do
     # shellcheck disable=SC2016
     envsubst '${VELA_API},${VELA_FEEDBACK_URL},{$VELA_DOCS_URL}' < "$f" > "$f".tmp && mv "$f".tmp "$f"
 done
-
-NGINX_CONF=/etc/nginx/conf.d/default.conf
-# shellcheck disable=SC2016
-envsubst '${VELA_API}' < "$NGINX_CONF" > "$NGINX_CONF".tmp && mv "$NGINX_CONF".tmp "$NGINX_CONF"
 
 exec "$@"
