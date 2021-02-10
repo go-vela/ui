b6ab336f3dfc159b9f448f12075cbb940bf52f97
davidvader <davelvader@gmail.com>
2021-02-10 13:10:46 -0600

    remove double paste

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,9 @@ do
     # shellcheck disable=SC2016
     envsubst '${VELA_API},${VELA_FEEDBACK_URL},{$VELA_DOCS_URL}' < "$f" > "$f".tmp && mv "$f".tmp "$f"
 done
-    envsubst '${VELA_API},${VELA_FEEDBACK_URL},{$VELA_DOCS_URL}' < "$f" > "$f".tmp && mv "$f".tmp "$f"
 
-# substitute config values
-NGINX_CONFIG=/etc/nginx/conf.d/default.conf
-
-envsubst '${VELA_API}' < "$NGINX_CONFIG" > "$NGINX_CONFIG".tmp && mv "$NGINX_CONFIG".tmp "$NGINX_CONFIG"
+NGINX_CONF=/etc/nginx/conf.d/default.conf
+# shellcheck disable=SC2016
+envsubst '${VELA_API}' < "$NGINX_CONF" > "$NGINX_CONF".tmp && mv "$NGINX_CONF".tmp "$NGINX_CONF"
 
 exec "$@"
