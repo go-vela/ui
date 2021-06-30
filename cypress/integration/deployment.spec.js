783b7cd7bcdf84a526f315d7e47d74d6ce67537d
James Christensen <j_christensen@cargill.com>
2021-06-30 09:54:18 -0500

    run prettier

--- cypress/integration/deployment.spec.js
+++ cypress/integration/deployment.spec.js
@@ -22,28 +22,39 @@ context('Deployment', () => {
 
     it('Add Parameter button should be disabled', () => {
       cy.get('[data-test=add-parameter-button]')
-        .should('exist').should('not.be.enabled')
+        .should('exist')
+        .should('not.be.enabled')
         .contains('Add');
     });
     it('Add Parameter should work as intended', () => {
       cy.get('[data-test=parameters-list]')
-        .should('exist').children().first().should('contain.text', 'No Parameters defined');
-      cy.get('[data-test=parameter-key-input]')
-        .should('exist').type('key1');
-      cy.get('[data-test=parameter-value-input]')
-        .should('exist').type('val1');
+        .should('exist')
+        .children()
+        .first()
+        .should('contain.text', 'No Parameters defined');
+      cy.get('[data-test=parameter-key-input]').should('exist').type('key1');
+      cy.get('[data-test=parameter-value-input]').should('exist').type('val1');
       cy.get('[data-test=add-parameter-button]')
-        .should('exist').should('be.enabled')
-        .contains('Add').click();
+        .should('exist')
+        .should('be.enabled')
+        .contains('Add')
+        .click();
       it('toast should show', () => {
         cy.get('[data-test=alerts]').should('exist').contains('Success');
       });
       cy.get('[data-test=parameters-list]')
-        .should('exist').children().first().children().first().should('contain.text', 'key1=val1');
+        .should('exist')
+        .children()
+        .first()
+        .children()
+        .first()
+        .should('contain.text', 'key1=val1');
       cy.get('[data-test=parameter-key-input]')
-        .should('exist').should('have.value', '');
+        .should('exist')
+        .should('have.value', '');
       cy.get('[data-test=parameter-value-input]')
-        .should('exist').should('have.value', '');
+        .should('exist')
+        .should('have.value', '');
     });
   });
 });
