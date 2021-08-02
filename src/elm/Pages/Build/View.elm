61e4dae1a58991d93d87b249c623575d348990f4
James Christensen <j_christensen@cargill.com>
2021-08-02 13:26:03 -0500

    change look to menu, add toast, close menu on action

--- src/elm/Pages/Build/View.elm
+++ src/elm/Pages/Build/View.elm
@@ -179,7 +179,8 @@ viewPreview msgs openMenu now zone org repo build =
         actionsMenu =
             details (buildMenuBaseClassList :: buildMenuAttributeList)
                 [ summary [ class "summary", Util.onClickPreventDefault (msgs.toggle build.id Nothing), Util.testAttribute "build-menu" ]
-                    [ FeatherIcons.chevronDown |> FeatherIcons.withSize 20 |> FeatherIcons.withClass "details-icon-expand" |> FeatherIcons.toHtml []
+                    [ text "Actions"
+                    , FeatherIcons.chevronDown |> FeatherIcons.withSize 20 |> FeatherIcons.withClass "details-icon-expand" |> FeatherIcons.toHtml []
                     ]
                 , ul [ class "build-menu", attribute "aria-hidden" "true", attribute "role" "menu" ]
                     [ li [ class "build-menu-item" ]
