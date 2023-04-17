295fd2fdccf444657b89fe39b8a8b6c326136113
dave vader <48764154+plyr4@users.noreply.github.com>
2023-04-17 09:27:06 -0500

    fix(deployments): fetch deployment when tab switching to PromoteDeployment page (#643)

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3313,6 +3313,9 @@ loadRepoSubPage model org repo toPage =
                     Pages.RepoSettings o r ->
                         ( model, getRepo model o r )
 
+                    Pages.PromoteDeployment o r deploymentNumber ->
+                        ( model, getDeployment model o r deploymentNumber )
+
                     -- page is not a repo subpage
                     _ ->
                         ( model, Cmd.none )
