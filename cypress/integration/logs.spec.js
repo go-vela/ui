e71d9a2c6b9dc3a26feef90a1cb4623cd694446a
davidvader <davelvader@gmail.com>
2023-06-02 16:25:35 -0500

    fix: cypress typo and code organization

--- cypress/integration/logs.spec.js
+++ cypress/integration/logs.spec.js
@@ -571,7 +571,7 @@ context(
     it('line should contain ansi color and link', () => {
       cy.get('[data-test=log-line-step-2-13]').within(() => {
         cy.get('[data-test=log-line-link]').should('exist');
-        cy.get('[class=ansi-magenta-fg]').should('exist');
+        cy.get('[class=ansi-magenta-bg]').should('exist');
         cy.get('[class=ansi-magenta-bg]').should(
           'have.css',
           'background-color',
