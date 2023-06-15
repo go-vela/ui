6a187c230ba8ed264ca119f33565e81fb5776f5f
davidvader <davelvader@gmail.com>
2023-06-15 09:33:28 -0500

    refactor: convert input to span

--- src/elm/Pages/Secrets/View.elm
+++ src/elm/Pages/Secrets/View.elm
@@ -8,8 +8,8 @@ module Pages.Secrets.View exposing (addSecret, editSecret, viewOrgSecrets, viewR
 
 import Errors exposing (viewResourceError)
 import FeatherIcons
-import Html exposing (Html, a, button, div, h2, input, span, td, text, tr)
-import Html.Attributes exposing (attribute, class, readonly, scope, size, type_, value)
+import Html exposing (Html, a, button, div, h2, span, td, text, tr)
+import Html.Attributes exposing (attribute, class, scope, type_)
 import Html.Events exposing (onClick)
 import Http
 import Pages.Secrets.Form exposing (viewAllowCommandCheckbox, viewEventsSelect, viewHelp, viewImagesInput, viewInput, viewNameInput, viewSubmitButtons, viewValueInput)
@@ -349,7 +349,7 @@ renderSecret type_ secret =
             , class "break-word"
             , Util.testAttribute <| "secrets-row-key"
             ]
-            [ readonlyInput "" secret.key
+            [ listItemView "" secret.key
             ]
         , td
             [ attribute "data-label" "type"
@@ -412,7 +412,7 @@ renderSharedSecret type_ secret =
             , class "break-word"
             , Util.testAttribute <| "secrets-row-key"
             ]
-            [ readonlyInput "" secret.key
+            [ listItemView "" secret.key
             ]
         , td
             [ attribute "data-label" "type"
@@ -455,8 +455,20 @@ renderListCell items none itemClassName =
             |> List.sort
             |> List.map
                 (\item ->
-                    readonlyInput itemClassName item
+                    listItemView itemClassName item
                 )
+
+
+{-| listItemView : takes classname, text and size constraints and renders a list element
+-}
+listItemView : String -> String -> Html msg
+listItemView className text_ =
+    div [ class className ]
+        [ span
+            [ class "list-item"
+            ]
+            [ text text_ ]
+        ]
 
 
 {-| updateSecretHref : takes secret and secret type and returns href link for routing to view/edit secret page
@@ -518,18 +530,6 @@ copyButton copyYaml =
             |> FeatherIcons.withSize 18
             |> FeatherIcons.toHtml []
         ]
-
-
-{-| readonlyInput : takes classname, text and size constraints and renders a readonly input element
--}
-readonlyInput : String -> String -> Html msg
-readonlyInput className text_ =
-    let
-        -- set a max size
-        size_ =
-            min 24 <| String.length text_
-    in
-    div [ class className ] [ input [ type_ "text", readonly True, size size_, value text_ ] [ text text_ ] ]
 
 
 
