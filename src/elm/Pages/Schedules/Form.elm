0f7cc2adeb265ae59267cc1b224f45561586cfaf
dave vader <48764154+plyr4@users.noreply.github.com>
2023-07-12 13:32:17 -0500

    tweak: use 12:00 AM
    Co-authored-by: Easton Crupper <65553218+ecrupper@users.noreply.github.com>

--- src/elm/Pages/Schedules/Form.elm
+++ src/elm/Pages/Schedules/Form.elm
@@ -70,7 +70,7 @@ viewEditForm model =
     in
     div [ class "schedule-form", class "edit-form" ]
         [ viewNameInput sm.form.name True
-        , viewValueInput sm.form.entry "0 0 * * * (runs at 00:00 AM in UTC)" (Util.toUtcString model.time)
+        , viewValueInput sm.form.entry "0 0 * * * (runs at 12:00 AM in UTC)" (Util.toUtcString model.time)
         , viewEnabledCheckbox sm.form
         , viewHelp
         , viewEditFormSubmitButtons sm
