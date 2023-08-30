691ac07a4632335eb8895813d6ae1d33ce57ec82
Claire.Nicholas <Z00B3R3@w6nxmk4t6t.target.com>
2023-08-30 11:06:48 -0500

    updating Form.elm

--- src/elm/Pages/Schedules/Form.elm
+++ src/elm/Pages/Schedules/Form.elm
@@ -175,13 +175,13 @@ viewEnabledCheckbox enableUpdate =
 viewBranchNameInput : String -> Bool -> Html Msg
 viewBranchNameInput val2 disable =
     section [ class "form-control", class "-stack" ]
-        [ label [ class "form-label", for <| "schedule-branch-name" ] 
-                [ strong [] [ text "Branch" ]
-                , span
-                    [ class "field-description" ]
-                    [ em [] [ text "(Leave blank to use default branch)" ]
-                    ]
+        [ label [ class "form-label", for <| "schedule-branch-name" ]
+            [ strong [] [ text "Branch" ]
+            , span
+                [ class "field-description" ]
+                [ em [] [ text "(Leave blank to use default branch)" ]
                 ]
+            ]
         , input
             [ disabled disable
             , value val2
