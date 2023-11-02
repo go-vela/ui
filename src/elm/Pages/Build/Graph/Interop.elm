d1e1a646103e2132f59cfd9991bf0f8f1f09eeae
davidvader <davelvader@gmail.com>
2023-11-02 11:47:23 -0500

    fix: cleaner render switch case

--- src/elm/Pages/Build/Graph/Interop.elm
+++ src/elm/Pages/Build/Graph/Interop.elm
@@ -13,28 +13,18 @@ import Vela exposing (encodeBuildGraphRenderData)
 renderBuildGraph : BuildModel.PartialModel a -> Bool -> Cmd msg
 renderBuildGraph model centerOnDraw =
     -- rendering the full graph requires repo, build and graph
-    case model.repo.repo of
-        Success r ->
-            case model.repo.build.build of
-                Success b ->
-                    case model.repo.build.graph.graph of
-                        Success g ->
-                            Interop.renderBuildGraph <|
-                                encodeBuildGraphRenderData
-                                    { dot = renderDOT model r b g
-                                    , buildID = b.id
-                                    , filter = model.repo.build.graph.filter
-                                    , showServices = model.repo.build.graph.showServices
-                                    , showSteps = model.repo.build.graph.showSteps
-                                    , focusedNode = model.repo.build.graph.focusedNode
-                                    , centerOnDraw = centerOnDraw
-                                    }
-
-                        _ ->
-                            Cmd.none
-
-                _ ->
-                    Cmd.none
+    case ( model.repo.repo, model.repo.build.build, model.repo.build.graph.graph ) of
+        ( Success r, Success b, Success g ) ->
+            Interop.renderBuildGraph <|
+                encodeBuildGraphRenderData
+                    { dot = renderDOT model r b g
+                    , buildID = b.id
+                    , filter = model.repo.build.graph.filter
+                    , showServices = model.repo.build.graph.showServices
+                    , showSteps = model.repo.build.graph.showSteps
+                    , focusedNode = model.repo.build.graph.focusedNode
+                    , centerOnDraw = centerOnDraw
+                    }
 
         _ ->
             Cmd.none
