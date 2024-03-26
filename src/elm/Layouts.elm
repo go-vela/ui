ca26accd084eae0c896e9a34ce5b37b0e004c0ef
davidvader <davelvader@gmail.com>
2024-03-26 11:06:24 -0500

    chore: realign types to match elm-land generation order

--- src/elm/Layouts.elm
+++ src/elm/Layouts.elm
@@ -13,9 +13,9 @@ import Layouts.Default.Repo
 
 type Layout msg
     = Default Layouts.Default.Props
+    | Default_Build (Layouts.Default.Build.Props msg)
     | Default_Org (Layouts.Default.Org.Props msg)
     | Default_Repo (Layouts.Default.Repo.Props msg)
-    | Default_Build (Layouts.Default.Build.Props msg)
 
 
 map : (msg1 -> msg2) -> Layout msg1 -> Layout msg2
@@ -24,11 +24,11 @@ map fn layout =
         Default data ->
             Default data
 
+        Default_Build data ->
+            Default_Build (Layouts.Default.Build.map fn  data)
+
         Default_Org data ->
-            Default_Org <| Layouts.Default.Org.map fn data
+            Default_Org (Layouts.Default.Org.map fn  data)
 
         Default_Repo data ->
-            Default_Repo <| Layouts.Default.Repo.map fn data
-
-        Default_Build data ->
-            Default_Build <| Layouts.Default.Build.map fn data
+            Default_Repo (Layouts.Default.Repo.map fn  data)
