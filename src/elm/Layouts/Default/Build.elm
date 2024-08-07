b83bba0661c7f2d4f5efc995c303cfd38fc3470d
david may <1301201+wass3r@users.noreply.github.com>
2024-08-07 19:37:35 +0000

    fix: reduce excessive API calls (#810)
    Co-authored-by: davidvader <davelvader@gmail.com>
    Co-authored-by: dave vader <48764154+plyr4@users.noreply.github.com>

--- src/elm/Layouts/Default/Build.elm
+++ src/elm/Layouts/Default/Build.elm
@@ -328,24 +328,42 @@ update props shared route msg model =
 
         -- REFRESH
         Tick options ->
+            let
+                isBuildRunning =
+                    case model.build of
+                        RemoteData.Success build ->
+                            build.finished == 0
+
+                        _ ->
+                            True
+
+                getRepoBuildsEffect =
+                    Effect.getRepoBuildsShared
+                        { pageNumber = Nothing
+                        , perPage = Nothing
+                        , maybeEvent = Nothing
+                        , org = props.org
+                        , repo = props.repo
+                        }
+
+                runEffects =
+                    if isBuildRunning then
+                        [ getRepoBuildsEffect
+                        , Effect.getBuild
+                            { baseUrl = shared.velaAPIBaseURL
+                            , session = shared.session
+                            , onResponse = GetBuildResponse
+                            , org = props.org
+                            , repo = props.repo
+                            , build = props.build
+                            }
+                        ]
+
+                    else
+                        [ getRepoBuildsEffect ]
+            in
             ( model
-            , Effect.batch
-                [ Effect.getRepoBuildsShared
-                    { pageNumber = Nothing
-                    , perPage = Nothing
-                    , maybeEvent = Nothing
-                    , org = props.org
-                    , repo = props.repo
-                    }
-                , Effect.getBuild
-                    { baseUrl = shared.velaAPIBaseURL
-                    , session = shared.session
-                    , onResponse = GetBuildResponse
-                    , org = props.org
-                    , repo = props.repo
-                    , build = props.build
-                    }
-                ]
+            , Effect.batch runEffects
             )
 
 
