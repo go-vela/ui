e6482a54e4d2e400c2001180b2d66e5690804275
davidvader <davelvader@gmail.com>
2025-01-07 12:36:55 -0600

    fix: comment spacing elm-format hack

--- src/elm/Utils/Warnings.elm
+++ src/elm/Utils/Warnings.elm
@@ -3,16 +3,31 @@ SPDX-License-Identifier: Apache-2.0
 --}
 
 
-module Utils.Warnings exposing (Warning, fromString)
+module Utils.Warnings exposing
+    ( Warning
+    , fromString
+    )
+
+{-|
+
+@docs Warning
+@docs fromString
+
+-}
+
+-- TYPES
+
 
 {-| Warning : an object that represents a point of focus.
 -}
-
-
 type alias Warning =
     { maybeLineNumber : Maybe Int
     , content : String
     }
+
+
+
+-- HELPERS
 
 
 {-| fromString : parses a warning string into a line number and a message.
